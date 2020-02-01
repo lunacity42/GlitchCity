26e5cbf64bd31ef67435b435bf5af8d2e671f3e1
lunacity42 <kati.alha@github.com>
2020-02-01 09:34:54 +0200

    template level and glitch

--- Player.gd
+++ Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-const SPEED = 20
-const MAX_SPEED = 200
+const SPEED = 10
+const MAX_SPEED = 100
 const FRICTION = 0.1
 
 var motion = Vector2()
@@ -9,32 +9,39 @@ var motion = Vector2()
 func _physics_process(delta):
 	update_movement()
 	move_and_slide(motion)
-	update_direction()
+	hack_glitch()
 
 func update_movement():
 	
 	if Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
 		motion.y = clamp(motion.y - SPEED, -MAX_SPEED, 0)
-#		if Input.is_action_pressed("move_left"):
-#			rotation_degrees = -135
-#		elif Input.is_action_pressed("move_right"):
-#			rotation_degrees = -45
-#		else:
-		rotation_degrees = -90
+		if Input.is_action_pressed("move_left"):
+			rotation_degrees = -135
+		elif Input.is_action_pressed("move_right"):
+			rotation_degrees = -45
+		else:
+			rotation_degrees = -90
 	elif Input.is_action_pressed("move_down") and not Input.is_action_pressed("move_up"):
 		motion.y = clamp(motion.y + SPEED, 0, MAX_SPEED)
-		rotation_degrees = 90
+		if Input.is_action_pressed("move_left"):
+			rotation_degrees = 135
+		elif Input.is_action_pressed("move_right"):
+			rotation_degrees = 45
+		else:
+			rotation_degrees = 90
 	else:
 		motion.y = 0
 
 	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
 		motion.x = clamp(motion.x - SPEED, -MAX_SPEED, 0)
-		rotation_degrees = 180
+		if not Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+			rotation_degrees = 180
 	elif Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
 		motion.x = clamp(motion.x + SPEED, 0, MAX_SPEED)
-		rotation_degrees = 0
+		if not Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+			rotation_degrees = 0
 	else:
 		motion.x = 0
 
-func update_direction():
+func hack_glitch():
 	pass
