d2736a3b128f0e8ff73025a92c6fd9e4dcee1b9b
lunacity42 <kati.alha@github.com>
2020-02-02 11:34:20 +0200

    third puzzle ready to be added

--- Glitches/GlitchTree.gd
+++ Glitches/GlitchTree.gd
@@ -1,2 +1,37 @@
 extends Area2D
 
+var in_glitch_area = false
+signal area_cleared
+
+func _on_GlitchTree_body_entered(body):
+	in_glitch_area = true
+	$GlitchHum.playing = true
+
+func _on_GlitchTree_body_exited(body):
+	in_glitch_area = false
+	$GlitchHum.playing = false
+	$CanvasLayer/GlitchPuzzle.hide()
+
+func _on_GlitchTree_input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and in_glitch_area:
+		print("yay")
+		$CanvasLayer/GlitchPuzzle.popup_centered()
+	
+func _input(event):
+	if event.is_action_pressed("interact") and in_glitch_area:
+		print("key pressed")
+		$CanvasLayer/GlitchPuzzle.popup_centered()
+
+func _on_GlitchPuzzle_code_correct():
+	$CanvasLayer/GlitchPuzzle.hide()
+	emit_signal("area_cleared")
+	queue_free()
+
+
+
+
+
+
+
+
+
