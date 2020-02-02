2fb62c4661e6ac6e54d2e87345cc467484fd743b
lunacity42 <kati.alha@github.com>
2020-02-02 12:24:45 +0200

    world is on fire

--- Levels/LevelStreet.gd
+++ Levels/LevelStreet.gd
@@ -4,3 +4,8 @@ func _on_GlitchSewer_area_cleared():
 	$NPC_child.visible = true
 	$GlitchTree.visible = true
 	$GlitchSewer.visible = false
+
+
+
+func _on_GlitchTree_area_cleared():
+	$TileMap2/Particles2D.visible = true
